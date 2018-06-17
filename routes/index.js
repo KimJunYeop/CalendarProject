b6b773b113d912913fb151ebe1975d109d42c7d0
Kim Jun-Yeop <wnsduq8282@gmail.com>
2018-06-17 23:48:29 +0900

    CSS 정돈

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 var redis = require('redis');
-var client = redis.createClient(6379,"127.0.0.1");
+var client = redis.createClient(6379, "127.0.0.1");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   client.hset("hash key", "hashtest 1", "some value", redis.print);
   client.hset('hash key', 'hashtest 2', 'some Test', redis.print);
   client.hkeys("hash key", function (err, replies) {
-  replies.forEach(function (reply, i) {
+    replies.forEach(function (reply, i) {
       console.log("    " + i + ": " + reply);
+    });
+    client.quit();
   });
-  client.quit();
-});
 
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/calendar',function(req,res){
+  console.log('calendar project started');
+
+  res.render('calendar');
+})
+
 
 module.exports = router;
